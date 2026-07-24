f210d1ff8efe31764fad927393bfb24e6f7f5b47
Omer Faruk Bakirci <ofaruk0204@gmail.com>
2026-07-25 02:36:52 +0300

    Initial Commit

---
+++ MenuMap/MenuMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuMapApp.swift
+//  MenuMap
+//
+//  Created by ÖMER on 25.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct MenuMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
